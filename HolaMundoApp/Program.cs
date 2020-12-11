b975d5a10ae789392245bddfd8b7fd237aeba6af
alexanderdeoz <sad.chinchin@yavirac.edu.ec>
2020-12-10 22:24:18 -0500

    calculadora Steven Chinchin

---
+++ HolaMundoApp/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Matematica;
+
+namespace HolaMundoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a mi calculadora");
+            Console.WriteLine("Ingresa un primer numero");
+            var num1= Console.ReadLine();
+            Console.WriteLine("Ingresa un segundo numero");
+            var num2 = Console.ReadLine();
+            Console.WriteLine("Que operacion quieres realizar");
+       Console.WriteLine("+ , - , * , / , Potencia , Raiz");
+          
+            var operacion = Console.ReadLine();
+            var calculadora = new Calculadora();
+            
+
+                if (operacion == "+")
+                {
+                    var respuesta = calculadora.Sumar(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la suma de los dos numeros es " + respuesta);
+                    Console.WriteLine("Que operacion quieres realizar");
+                }
+                if (operacion == "-")
+                {
+                    var respuesta = calculadora.Restar(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la resta de los dos numeros es " + respuesta);
+                }
+
+                if (operacion == "*")
+                {
+                    var respuesta = calculadora.Multiplicar(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la multiplicacion de los dos numeros es " + respuesta);
+                }
+                if (operacion == "/")
+                {
+                    var respuesta = calculadora.Dividir(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la division de los dos numeros es " + respuesta);
+                }
+                if (operacion == "potencia")
+                {
+                    Console.WriteLine("el primer numero se tomara como exponente y el segundo como base");
+                    var respuesta = calculadora.Potencia(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la potencia es  " + respuesta);
+                }
+                if (operacion == "Raiz")
+                {
+
+                    var conversion = int.Parse(num1);
+                    var conversion2 = int.Parse(num2);
+                    double result = Math.Sqrt(conversion);
+                    double result2 = Math.Sqrt(conversion2);
+                    Console.WriteLine("la raiz del primer numero es =" + result);
+                    Console.WriteLine("la raiz del segundo numero es =" + result2);
+                }
+                 }
+
+            
+
+
+           
+            /*Console.WriteLine("Ingresa el primer numero");
+            var numero1 =Console.ReadLine();
+            Console.WriteLine("Ingresa un segundo numero");
+            var numero2 = Console.ReadLine();
+            var calculadora = new Calculadora();
+            var respuesta = calculadora.Sumar(int.Parse(numero1) , int.Parse(numero2));
+            Console.WriteLine("la suma de los dos numeros es " + respuesta);*/
+
+            /* Console.WriteLine("Ingresa tu nombre");
+             var entrada= Console.ReadLine();
+             Console.WriteLine("Ingresaste" + entrada);
+             var salida=Console.ReadLine();
+             if (salida =="salir") 
+             {
+                 Console.Read();
+             }*/
+
+        }
+       
+    
+}
---
+++ Matematica/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Matematica
+{
+    public class Calculadora
+    {
+        public int Sumar(int sumando1, int sumando2)
+        {
+            return sumando1 + sumando2;
+
+        }
+
+        public int Restar(int sumando1, int sumando2) 
+        {
+            return sumando1 - sumando2;
+        }
+
+        public int Multiplicar(int sumando1, int sumando2)
+        {
+            return sumando1 * sumando2;
+        }
+
+        public int Dividir(int sumando1, int sumando2)
+        {
+            return sumando1 / sumando2;
+        }
+
+        public int Potencia(int sumando1, int sumando2)
+        {
+            var contador = 0;
+            for (var i =0;i<=sumando1;i++) 
+            {
+                contador = sumando2 * sumando2;
+            }
+            return contador;
+        }
+
+   
+
+
+
+
+    }
+}
